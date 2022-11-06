22f6208d4ecfc81cd91adf21edcbf2621b497352
popei8492 <116310002+popei8492@users.noreply.github.com>
2022-11-06 14:20:00 -0500

    Algebratic Equations with Explicit Mulitplication

---
+++ pcep_2.4.1.10 LAB Operators and expressions.py
@@ -0,0 +1,5 @@
+x = -1       # hardcode your test data here
+x = float(x)                                        #  x = 0,  y = -1.0
+# write your code here         #test data-->        #  x = 1,  y = 3.0
+y = 3*x**3 - 2*x**2 + 3*x - 1                       #  x = -1, y = -9.0
+print("y =", y)
