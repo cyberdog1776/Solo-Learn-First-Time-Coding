7c6634e2e777abd51d456a32416bf5d7bf5c812c
popei8492 <116310002+popei8492@users.noreply.github.com>
2022-11-06 11:22:36 -0500

    list & loops end of modules

---
+++ solo_Functions_Sum of Consecutive Numbers.py
@@ -0,0 +1,8 @@
+#
+N = int(input())            #how many times to iterate 
+sum=0                       #starting with zero
+for i in range(1,N+1):      #from 1 to num of iterations
+    sum+=i                  #add 1 every time to sum until
+print(sum)                      #reaching 6 iterations, then
+                                #add 1 more
+                             
