baf17d2ab7941fb8e375bd73f0facbe0b953f46e
popei8492 <116310002+popei8492@users.noreply.github.com>
2022-12-01 14:17:54 -0500

    Add files via upload

---
+++ Fruit Vending Machine.py
@@ -0,0 +1,25 @@
+fruits = ["apple", "cherry", "banana", "kiwi", "lemon", "pear", "peach", "avocado"]
+num = int(input())
+#your code goes here
+if num < 0 or num >7:
+    print('Wrong number')
+else:
+    print(fruits[num])
+'''elif num == 0:            First Solution-->Lots of code.
+    print(fruits[0])
+elif num == 1:
+    print(fruits[1])
+elif num == 2:
+    print(fruits[2])
+elif num == 3:
+    print(fruits[3])
+elif num == 4:
+    print(fruits[4])
+elif num == 5:
+    print(fruits[5])
+elif num == 6:
+    print(fruits[6])
+elif num == 7:
+    print(fruits[7])
+elif num == 8:
+    print(fruits[8])'''
